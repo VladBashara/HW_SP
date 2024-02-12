745c45d59746018938f12b198f4d64193e82ae30
VladBashara <bashara2003@gmail.com>
2024-02-12 16:46:41 +0300

    update cross dir

--- cross/test.js
+++ cross/test.js
@@ -1,10 +1,10 @@
 // let promise = new Promise(function(resolve, reject) {
 //     console.log("Make Promise");
 //     setTimeout(() => { reject(new Error("adads")); }, 10);
-//     setTimeout(() => { console.log("promise.status: " + this.status); }, 1000);
+//     setTimeout(() => { console.log("promise.status: " + this.status); }, 10000);
 //     setTimeout(() => { console.log("promise.result: " + this.result); }, 1000);
 // });
-// promise.finally(
+// promise.then(
 //     function() { console.log("Make error "); }
 // ).catch(function() { console.log("End"); });
 
@@ -19,3 +19,14 @@
 //                                                     } else {
 //                                                         console.log("result.reason " + result.reason);
 //                                                     } }); });
+
+// let promise = Promise.resolve();
+// promise.then(() => {
+//     console.log("First then"); setTimeout(()=>{}, 100); 
+// });
+// promise.then(setTimeout(()=>{console.log("Second then")}, 0));
+// promise.then(console.log("Fourth then"));
+// promise.then(() => {
+//     console.log("Third then"); setTimeout(()=>{}, 0); 
+// });
+// console.log("EXIT");
