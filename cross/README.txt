ba18262706d7af89c22a8f65f64b0079b98c52e0
VladBashara <bashara2003@gmail.com>
2024-02-16 01:06:43 +0300

    update

--- cross/README.txt
+++ cross/README.txt
@@ -1,7 +1,7 @@
 Команда для запуска:
 
-sh clean.sh && nodejs parent.js array 2 0
-clean.bat && nodejs parent.js array 2 0
+sh clean.sh && nodejs parent.js array 2
+clean.bat && nodejs parent.js array 2
 
 python3 gen.py <filename> <left_edge> <right_edge> <NumCount>
 python3 gen.py generated 0 20 5
--- cross/testchild.js
+++ cross/testchild.js
@@ -1,14 +1,13 @@
 const { exit, argv } = require('process');
-const cluster = require('cluster');
 const fs = require('fs');
-// console.log("Child PID: " + process.pid);
-// console.log(argv[2], argv[3]);
 const file_num = argv[2];
 const delay = argv[3];
-// console.log("./splitted_arrays/" + file_num);
+console.log("PID", process.pid);
 const data = fs.readFileSync("./splitted_arrays/" + file_num, 'utf-8'); // exit(1)
 let data_lst = data.split(' ');
 let result = 0;
 data_lst.forEach((num) => { result += Number(num); });
 fs.writeFileSync("./summed_arrays/" + file_num, String(result)); // exit(1)
-exit(0);
+setTimeout(() => {
+    exit(0); 
+}, 10000);
