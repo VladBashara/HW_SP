698ef0514efdb75cec7a5c2a1ebfeb21a05fc96e
VladBashara <bashara2003@gmail.com>
2024-02-16 00:17:42 +0300

    works

--- cross/testchild.js
+++ cross/testchild.js
@@ -1,14 +1,14 @@
 const { exit, argv } = require('process');
 const cluster = require('cluster');
 const fs = require('fs');
-console.log("Child PID: " + process.pid);
-console.log(argv[2], argv[3]);
+// console.log("Child PID: " + process.pid);
+// console.log(argv[2], argv[3]);
 const file_num = argv[2];
 const delay = argv[3];
-console.log("./splitted_arrays/" + file_num);
+// console.log("./splitted_arrays/" + file_num);
 const data = fs.readFileSync("./splitted_arrays/" + file_num, 'utf-8'); // exit(1)
 let data_lst = data.split(' ');
 let result = 0;
 data_lst.forEach((num) => { result += Number(num); });
 fs.writeFileSync("./summed_arrays/" + file_num, String(result)); // exit(1)
-setTimeout(() => { console.log("exit(0)"); exit(0); }, 10);
+exit(0);
--- cross/testparent.js
+++ cross/testparent.js
@@ -17,38 +17,31 @@ function call_subprocesses(N, delay) {
     let child_proc = Array(N);
     for (let i = 0; i < N; i++) {
         child_proc[i] = child.fork("./testchild.js", [i+1, delay]);
-        console.log("PROC " + child_proc[i].pid);
+        // console.log("PROC " + child_proc[i].pid);
     }
     return child_proc;
 }
 
 async function wait_subprocesses(N, child_proc) {
+    // return Promise.resolve("99");
+//     Promise.all(child_proc.map(x => {console.log("PRINTITN");
+//     x.on("exit", () => {console.log("SUPER"); Promise.resolve("resolved");});
+// }
+//     )).then((z) => {console.log("return Promise.resolve('') ", z); return Promise.resolve('21');},
+//             () => {return Promise.resolve('22'); throw new Error("ERROR 32");}
+//         ).catch(() => {console.log("ERROR 33");}).finally(() => {return Promise.resolve('end')});
+    // return child_proc.map( x => {console.log("PRINTITN ", x.pid);
+    // x.on("exit", () => {console.log("SUPER"); return Promise.resolve("resolved");})});
+    // let smth = await child_proc.map(async function(x) {console.log("PRINTITN ", x.pid);
+    // let counter = 0;
+    // let param = await x.on("exit", () => {console.log("PRINTITN2 "); counter = 1;});
+    // if (counter == 1) { console.log("SUPER"); return Promise.resolve("resolved"); }
+    // });
+    // console.log("log ", smth);
+    // return smth;
     let counter = 0;
-    console.log("\nHERE\n");
-    
-    Promise.all(child_proc.map(x => {new Promise(
-        () => {
-            x.on("exit", (code) => {
-                console.log("EXIT ");
-                if (code == 0) { resolve(''); } else { reject(''); }
-                // resolve('');
-            })})}
-    )).finally(() => {
-        return Promise.resolve('');
-    }).catch(() => {
-        return Promise.reject('');
-    });
-    // for (let i = 0; i<N; i++) {
-    //     child_proc[i].on("exit", (code, signal) => {
-    //         if (code == 0) {
-    //             counter++
-    //         } else { return Promise.reject('bad'); }
-    //         if (counter == N) {
-    //             return Promise.resolve('good');
-    //         }
-    //     })
-    // }
-    // return Promise.reject('bad');
+    x.on("exit", () => {console.log("PRINTITN2 "); counter++;});
+    if (counter== N) { return Promise.resolve(1);}
 }
 
 // function wait_subprocesses(N, child_proc) {
@@ -70,13 +63,13 @@ async function wait_subprocesses(N, child_proc) {
 //     if (code == 0) { resolve(''); } else { reject(''); }
 // })})).then().catch();
 function summation(N) {
-    let folder = './summed_arrays/';
+    let folder = 'summed_arrays/';
     let result = 0;
     for (let i = 0; i < N; i++) {
         let buf = fs.readFileSync(folder + (i+1), 'utf-8');
         result += Number(buf);
     }
-    console.log("Result = " + result);
+    console.log("Result = " + result + "\n");
 }
 
 const { exit, argv } = require('process');
@@ -97,10 +90,10 @@ let N = argv[3];
 //         })
 // });
 // promise.finally( function() { console.log("CONSOLE"); } ).finally( function() { console.log("CONSOLE"); } );
-
+console.log();
 const data = fs.readFileSync("./array", 'utf-8');
 let data_lst = data.split(' ');
-console.log("data: " + data);
+// console.log("data: " + data);
 
 if (data_lst[data_lst.length-1] == '') {
     data_lst.splice(data_lst.length-1, 1);
@@ -114,15 +107,15 @@ if (N > M/2) {
 let M_prcs = div(M , N);
 let M_last_prcs = M - (N-1)*div(M , N);
 
-console.log("M - data: " + M + "\n");
-console.log("M_prcs: " + M_prcs + "\n");
-console.log("M_last_prcs: " + M_last_prcs + "\n");
-console.log("N - subproccesses: " + N + "\n\n");
-console.log("data_lst.length: ", data_lst.length);
+console.log("M - data: " + M);
+console.log("M_prcs: " + M_prcs);
+console.log("M_last_prcs: " + M_last_prcs);
+console.log("N - subproccesses: " + N);
+// console.log("data_lst.length: ", data_lst.length);
 
 // fs.writeFileSync(`./splitted_arrays/${N}`, 'kek');
-console.log(data_lst);
-console.log(data_lst.slice(0, 2));
+// console.log(data_lst);
+// console.log(data_lst.slice(0, 2));
 let i = 0;
 for (i; i < N-1; i++) {
     write_numbers_to_file(`./splitted_arrays/${i+1}`, data_lst.slice(i*M_prcs, (i+1)*M_prcs));
@@ -130,4 +123,30 @@ for (i; i < N-1; i++) {
 write_numbers_to_file(`./splitted_arrays/${N}`, data_lst.slice(i*M_prcs, data_lst.length));
 
 let child_proc = call_subprocesses(N, 1);
-wait_subprocesses(N, child_proc).then(() => {summation(N)}).catch((error) => {console.log("ERROR ", error)});
+// let ggg = wait_subprocesses(N, child_proc);
+// ggg.then(res => {console.log("res ", res)});
+// let smth = await child_proc.map(x => {console.log("PRINTITN ", x.pid);
+// x.on("exit", () => {console.log("SUPER"); return Promise.resolve("resolved");});
+// // return Promise.resolve("resolved");
+// });
+// let smth = wait_subprocesses(N, child_proc);
+// let smth = child_proc.map( x => {console.log("PRINTITN ", x.pid);
+// let counter = 0;
+// x.on("exit", () => {counter = 1;})
+// if (counter == 1) { console.log("SUPER"); return Promise.resolve("resolved"); }
+// });
+// let smth = wait_subprocesses(N, child_proc);
+// console.log("asdasd ", smth);
+// let smth = [
+//     new Promise(resolve => setTimeout(() => resolve(1), 3000))
+//   ];
+let counter = 0;
+child_proc.forEach(x => {
+    x.on("exit", () => {
+    counter++;
+    if (counter == N) {
+        summation(N);
+    }});
+})
+// Promise.all(smth).then(smth => {console.log("smth ", smth)});
+// Promise.all(smth).then(() => {console.log("YEEAH");}).catch((error) => {console.log("ERROR ", error)});
