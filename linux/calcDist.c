215d5e8646d5cc8f53726be476d7fa53d37c338e
VladBashara <bashara2003@gmail.com>
2024-02-20 18:58:11 +0300

    update calcDist

--- linux/calcDist.c
+++ linux/calcDist.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-void calcDist(int const * const M, int* const N, int* const M_prcs, int* const M_last_prcs) {
+void calcDist(int* N, int* M, int* M_prcs, int* M_last_prcs) {
     if ((*N) > (*M)/2) {
         (*N) = (*M)/2;
         printf("Warning: N = M//2\n");
     }
-    (*M_prcs) = (*M) / (*N);
-    (*M_last_prcs) = (*M) - ((*N)-1)*((*M) / (*N));
-    
-    // printf("M - data size: %d\n", *M);
-    // printf("N - child processes: %d\n", *N);
-    // printf("M_prcs: %d\n", *M_prcs);
-    // printf("M_last_prcs: %d\n", *M_last_prcs);
+    *M_prcs = (*M) / (*N);
+    *M_last_prcs = (*M) - ((*N)-1)*((*M) / (*N));
+
+    printf("M - data size: %d\n", *M);
+    printf("N - child processes: %d\n", *N);
+    printf("M_prcs: %d\n", *M_prcs);
+    printf("M_last_prcs: %d\n", *M_last_prcs);
 }
