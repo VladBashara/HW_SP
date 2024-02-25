2066d23f65f789de5c32e6e76437ff0c94cd2d6d
VladBashara <bashara2003@gmail.com>
2024-02-25 16:03:20 +0300

    add windows program

---
+++ windows/main.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+// #include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+// #include <sys/wait.h>
+#include <synchapi.h>
+#include <ctype.h>
+// #include <pthread.h>
+#include <Windows.h>
+#include "calcDist.c"
+#include "itoa.c"
+#include "module.c"
+
+
+int read_file_to_buf(FILE* fp, long file_size, char** buf) {
+    *buf = malloc((file_size+1) * sizeof(char));
+    fread(*buf, 1, file_size, fp);
+    if (ferror(fp)) {
+        free(*buf);
+        print_error_msg("Can`t read the file");
+        return 1;
+    }
+    (*buf)[file_size] = '\0';
+    if (fclose(fp)) {
+        free(*buf);
+        print_error_msg("Can`t close the file");
+        return 1;
+    }
+    return 0;
+}
+
+struct ARG {
+    int delay;
+    char* substr;
+};
+
+double sum = 0;
+// static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+CRITICAL_SECTION cs;
+DWORD th_main(LPVOID arg) {
+    Sleep(1000*((struct ARG*)arg)->delay);
+    char* substrs = (char*)(((struct ARG*)arg)->substr);
+    int len = 0;
+    long* size_list = NULL;
+    char** pos_list = NULL;
+    split_str(substrs, strlen(substrs), &pos_list, &size_list, &len);
+    // pthread_mutex_lock(&mutex);
+    EnterCriticalSection(&cs);
+    for (int i = 0; i < len; i++) {
+        char* val = malloc(size_list[i]);
+        strncpy(val, pos_list[i], size_list[i]);
+        sum += atof(val);
+        printf("THREAD atof %f\n", atof(val));
+        free(val);
+    }
+    // pthread_mutex_unlock(&mutex);
+    LeaveCriticalSection(&cs);
+    // int* r = malloc(sizeof(int));
+    // *r = 12;
+    // pthread_exit(r);
+    ExitThread(0);
+}
+
+#define MACHINE_WORD_SIZE 64
+#if MACHINE_WORD_SIZE == 64
+    # define MACHINE_WORD_TYPE long long
+#else
+    #define MACHINE_WORD_TYPE int
+#endif
+
+void get_substr(char*** substr, char** buf, long file_size, int N, int M_prcs, int M_last_prcs) {
+    if ((*buf)[file_size-1] != ' ') {
+        char* new_buf = (char*)malloc((file_size+ 1 + 1) * sizeof(char));
+        strncpy(new_buf, *buf, file_size);
+        strncpy(&new_buf[file_size], " ", 1);
+        strncpy(&new_buf[file_size+1], "\0", 1);
+        free(*buf);
+        *buf = (char*)malloc((file_size + 1 + 1) * sizeof(char));
+        strncpy(*buf, new_buf, file_size + 1 + 1);
+        free(new_buf);
+    }
+    MACHINE_WORD_TYPE idx = (MACHINE_WORD_TYPE)(*buf)-1;
+    MACHINE_WORD_TYPE start = (MACHINE_WORD_TYPE)(*buf);
+    MACHINE_WORD_TYPE const start_const = (MACHINE_WORD_TYPE)(*buf);
+    int jmp_counter = 0;
+    for (int j = 0; j < N; j++) {
+        if (j == N-1) { jmp_counter = M_last_prcs; } else { jmp_counter = M_prcs; }
+        for (int i = 0; i < jmp_counter; i++) {
+            idx = (MACHINE_WORD_TYPE)strchr(&(*buf)[idx+1-start_const], (long)' ');
+        }
+        strncpy((*substr)[j], &((*buf)[start-start_const]), idx-start);
+        start = idx + 1;
+    }
+}
+
+#define ARGC 4
+int main(int argc, char* argv[]) {
+
+    if (argc != ARGC) {
+        printf("ERROR: argc != %d\n", ARGC);
+        return 1;
+    }
+
+    FILE* fp = fopen(argv[1], "rb");
+    if (fp == NULL) {
+        print_error_msg("file doesn`t exist");
+        return 1;
+    }
+    printf("PID: %d\n", GetCurrentProcessId());
+    char* delay_str = argv[3];
+
+    long file_size = get_file_size(fp);
+    char* buf = NULL;
+    if (read_file_to_buf(fp, file_size, &buf)) { free(buf); return 1; }
+
+    int N = atoi(argv[2]);
+    int M = count_str(buf, '.');
+    if (M < 2) { free(buf); printf("ERROR: M < 2\n"); return 1; }
+    int M_prcs;
+    int M_last_prcs;
+    calcDist(&N, &M, &M_prcs, &M_last_prcs);
+    
+    char** substr = malloc(N * sizeof(char*));
+    for (int i = 0; i < N; i++) {
+        substr[i] = calloc(file_size, sizeof(char));
+    }
+
+    get_substr(&substr, &buf, file_size, N, M_prcs, M_last_prcs);
+    
+    // pthread_t* id = malloc(N * sizeof(pthread_t));
+    InitializeCriticalSection(&cs);
+    struct ARG* arg = malloc(N * sizeof(struct ARG));
+    HANDLE* th_handle = malloc(N * sizeof(HANDLE));
+    for (int i = 0; i<N; i++) {
+        arg[i].delay = atoi(delay_str);
+        arg[i].substr = substr[i];
+        // pthread_create(&id[i], NULL, th_main, (void *)&(arg[i]));
+        DWORD id;
+        th_handle[i] = CreateThread(NULL, 0, th_main, &arg[i], 0, &id);
+    }
+    printf("LOL\n");
+    Sleep(1000*atoi(delay_str));
+    for (int i = 0; i<N; i++) {
+        // pthread_join(id[i], &th_ret);
+        WaitForSingleObject(th_handle[i], INFINITE);
+    }
+    for (int i = 0; i<N; i++) {
+        free(substr[i]);
+    }
+    free(substr);
+    free(buf);
+    free(arg);
+    free(th_handle);
+    // free(id);
+    DeleteCriticalSection(&cs);
+    printf("\nsum is %f\n", sum);
+    return 0;
+}
---
+++ windows/module.c
@@ -0,0 +1,120 @@
+#include <synchapi.h>
+#include <windows.h>
+
+int getIntSize(int n) {
+    if (n == 0) { return 1; }
+    int i = 0;
+    while (n != 0) {
+        n = (int)(n / 10);
+        i++;
+    }
+    return i;
+}
+
+void print_error_msg(char* add_msg) {
+    LPSTR message;
+    FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER, NULL, GetLastError(),
+    MAKELANGID(LANG_ENGLISH, SUBLANG_DEFAULT), (LPSTR)&message, 0, NULL);
+    printf("%d : (%s) %s\n", GetLastError(), add_msg, message);
+}
+
+long get_file_size(FILE* fp) {
+    fseek(fp, 0, SEEK_CUR);
+    long offset = ftell(fp);
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+    fseek(fp, offset, SEEK_SET);
+    return size;
+}
+
+int count_str(char* buf, char symbol) {
+    int i = 0;
+    char* ptr = NULL;
+    ptr = strchr(buf, (int)symbol);
+    if (ptr == NULL) { return 0; }
+    buf = &ptr[1];
+    while (ptr != NULL) {
+        ptr = strchr(buf, (int)symbol);
+        buf = &ptr[1];
+        i++;
+    }
+    return i;
+}
+
+void split_str(char* buf, long buf_size, char*** pos_list, long** size_list, int* len) {
+    *len = count_str(buf, '.');
+    *size_list = malloc(*len * sizeof(long));
+    *pos_list = malloc(*len * sizeof(char*));
+
+    long long start_pos = (long long)buf;
+    char symbol = ' ';
+    char* ptr = NULL;
+    for (int i = 0; i < *len; i++) {
+        (*pos_list)[i] = buf;
+        ptr = strchr(buf, (long long)symbol);
+        if (ptr == NULL) {
+            (*size_list)[i] =  buf_size-(long long)buf+start_pos;
+        } else {
+            (*size_list)[i] = (long long)ptr-(long long)buf;
+            buf = &ptr[1];
+        }
+    }
+}
+    
+int write_slices_to_files(char* buf, long file_size, int N, int M_prcs, int M_last_prcs) {
+    int len = 0;
+    long* size_list = NULL;
+    char** pos_list = NULL;
+    split_str(buf, file_size, &pos_list, &size_list, &len);
+    int counter = 0;
+
+    for (int j = 0; j < N-1; j++) {
+        char file[] = "splitted_arrays/";
+        FILE* fp = fopen(strcat(file, i_to_a(j+1)), "wb"); if (fp == NULL) { return 1; }
+        for (int i = 0; i < M_prcs; i++) {
+            fwrite(pos_list[counter], 1, size_list[counter], fp); if (ferror(fp)) { return 1; }
+            counter++;
+            if (i != M_prcs-1) { fwrite(" ", 1, 1, fp); }
+        }
+        if (fclose(fp)) { return 1; }
+    }
+    char file[] = "splitted_arrays/";
+    FILE* fp = fopen(strcat(file, i_to_a(N)), "wb");
+    for (int i = 0; i < M_last_prcs; i++) {
+        fwrite(pos_list[counter], 1, size_list[counter], fp); if (ferror(fp)) { return 1; }
+        counter++;
+        if (i != M_last_prcs-1) { fwrite(" ", 1, 1, fp); if (ferror(fp)) { return 1; } }
+    }
+    free(size_list);
+    if (fclose(fp)) { return 1; }
+    return 0;
+}
+
+void waiting_all_proccesses(int N, char* delay_str,  HANDLE* proc_descriptors,  HANDLE* thr_descriptors) {
+    int ret_code;
+    Sleep(1000*4*atoi(delay_str));
+    DWORD exit_code = 0;
+    for (int j = 0; j < N; j++) {
+        if (WaitForSingleObject(proc_descriptors[j], INFINITE) == WAIT_FAILED) { printf("ERROR 102\n"); }
+        if (!GetExitCodeProcess(proc_descriptors[j], &exit_code)) { printf("ERROR 103\n"); }
+        if (exit_code == 1) { printf("ERROR 104\n"); }
+        CloseHandle(proc_descriptors[j]);
+        CloseHandle(thr_descriptors[j]);
+    }
+}
+
+void calling_proccesses(int N, char* delay_str, HANDLE* proc_descriptors, HANDLE* thr_descriptors) {
+    for (int i = 0; i < N; i++) {
+        PROCESS_INFORMATION pi;
+        STARTUPINFO si;
+        GetStartupInfo(&si);
+        char* argv = calloc((strlen(i_to_a(i+1)) + 1 + strlen(delay_str) + 1), sizeof(char));
+        strcat(argv, i_to_a(i+1));
+        strcat(argv, " ");
+        strcat(argv, delay_str);
+        BOOL res = CreateProcess( "p_child.exe", (LPSTR)argv, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
+        free(argv);
+        proc_descriptors[i] = pi.hProcess;
+        thr_descriptors[i] = pi.hThread;
+    }
+}
